4948f6234d55f480cbdd0cdb16abfe017b7c7109
Hong Hieu <Nguyenhonghieuit@gmail.com>
2021-11-23 21:31:09 +0700

    typescript_123

--- test/advance_test.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("submit").onclick = function() {
-    var fragment = document.createDocumentFragment();
-    fragment.appendChild(document.getElementById('submit'));
-    document.getElementById('container').appendChild(fragment);
-}
